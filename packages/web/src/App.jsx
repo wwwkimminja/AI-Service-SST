d37161fc25669029478c3ddcb8fbc8f12ad158de
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2024-11-26 16:40:35 +0900

    root page

--- packages/web/src/App.jsx
+++ packages/web/src/App.jsx
@@ -1,11 +1,24 @@
+import { useNavigate } from "react-router-dom"
+import Header from "./components/Header/Header"
+import './App.css'
+import heroImage from './assets/images/hero-image.png'
+
 
 
 function App() {
 
+  const navigate=useNavigate()
+  
   return (
-    <>
- Hello!
-    </>
+    <div className="App">
+    <Header subHeading={'Let AI Create a Stunning Portrait of Your Pet'}/>
+    <div style={{marginTop:'30px'}}>
+      <button onClick={()=>navigate('/start')}>GET STARTED</button>
+    </div>
+    <div className="hero-image-container">
+      <img className='hero-image' src={heroImage} alt="hero-image" />
+    </div>
+    </div>
   )
 }
 
